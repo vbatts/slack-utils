98f63405a21272a4aada16f4bfbd477415686018
Vincent Batts <vbatts@hashbangbash.com>
2010-09-08 12:29:57 -0400

    lots of reorganization. now builds in classic tarball fashion

---
+++ bundle.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CWD=$(pwd)
+TMP=${TMP:-/tmp}
+
+if [ -r $CWD/local.conf ] ;then
+	. $CWD/local.conf
+else
+	echo "ERROR: local.conf not found"
+	exit 1
+fi
+
+rm -rf $TMP/${PRGNAM}-${VERSION}
+cp -a $CWD/src $TMP/${PRGNAM}-${VERSION}
+(cd $TMP && tar zcvf $CWD/${PRGNAM}-${VERSION}.tar.gz ${PRGNAM}-${VERSION})
