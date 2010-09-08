15f75889fbf10a0af0745ec14a855fe7b65bbfd9
Vincent Batts <vbatts@hashbangbash.com>
2010-09-08 16:13:25 -0400

    fixing slf, and starting ruby implementation of slo

--- src/lib/slack-utils.rb
+++ src/lib/slack-utils.rb
@@ -4,8 +4,9 @@
 # Copyright 2009, 2010 Vincent Batts, http://hashbangbash.com/
 
 # Variables
-@pd = '/var/log/packages' # TODO this should be put to a conf file at some point
-@pa = Dir.entries(@pd)
+@installed_packages_dir = '/var/log/packages' # TODO this should be put to a conf file at some point
+@removed_packages_dir = '/var/log/removed_packages/' # TODO this should be put to a conf file at some point
+@packages_array = Dir.entries(@installed_packages_dir)
 @me = File.basename($0)
 @st = "\033[31;1m" # TODO These should be put to a flag at some point
 @en = "\033[0m"
@@ -17,17 +18,15 @@ class Slackware
 	def initialize
 		@name = @file = @path = @time = ""
 	end
-
-
 end
 
 # Functions
 def sl_path(package)
-	File.absolute_path(File.join(@pd, '/', package))
+	File.absolute_path(File.join(@installed_packages_dir, '/', package))
 end
 
 def is_sl_pd?(some_path)
-	if (some_path == @pd || some_path == "/var/log")
+	if (some_path == @installed_packages_dir || some_path == "/var/log")
 		true
 	else
 		false
@@ -36,25 +35,25 @@ end
 
 def slp
 	if ARGV.count == 0
-		@pa.each {|pkg|
+		@packages_array.each {|pkg|
 			puts pkg
 		}
 	else
 		ARGV.each {|arg|
-			puts @pa.grep(/#{arg}/)
+			puts @packages_array.grep(/#{arg}/)
 		}
 	end
 end
 
 def slt
 	if ARGV.count == 0
-		@pa.each {|pkg|
+		@packages_array.each {|pkg|
 			file_time = File.mtime(sl_path(pkg))
 			puts "#{pkg}:\s#{file_time}"
 		}
 	else
 		ARGV.each {|arg|
-			@pa.grep(/#{arg}/).each {|pkg|
+			@packages_array.grep(/#{arg}/).each {|pkg|
 				next if is_sl_pd?(sl_path(pkg))
 				file_time = File.mtime(sl_path(pkg))
 				puts "#{pkg}:\s#{file_time}"
@@ -70,7 +69,7 @@ def slf
 	else
 		ARGV.each {|arg|
 			r = Regexp::new(/#{arg}/)
-			@pa.each {|pkg|
+			@packages_array.each {|pkg|
 				p = sl_path(pkg)
 				next if is_sl_pd?(p)
 				f = File.open(p)
@@ -91,7 +90,7 @@ def sll
 		puts "#{@me}: what package do you want to list?"
 	else
 		ARGV.each {|arg|
-			@pa.grep(/#{arg}/).each {|pkg|
+			@packages_array.grep(/#{arg}/).each {|pkg|
 				p = sl_path(pkg)
 				next if is_sl_pd?(p)
 				system("tail +$(expr $(grep -n ^FILE #{p} | cut -d : -f 1 ) + 2) #{p} ")
@@ -102,5 +101,17 @@ end
 
 # XXX stub for slack-utils orpaned .new files (to be written in ruby)
 def slo
+	new_pat = Regexp.new(/\.new$/)
+	removed_packages_array = Dir.entries(@removed_packages_dir)
+	removed_packages_array.each {|pkg|
+		file_path = File.absolute_path(File.join(@removed_packages_dir, '/', pkg))
+		if (file_path == @removed_packages_dir || file_path == "/var/log" || file_path == "/var/log/removed_packages")
+			next
+		end
+		file = File.open(file_path)
+		file.each {|line|
+			puts line if new_pat.match(line)
+		}
+	}
 end
 
