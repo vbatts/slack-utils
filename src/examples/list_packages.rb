bab5e2b6165de544193e342a07c7c8f20cdf7522
Vincent Batts <vbatts@hashbangbash.com>
2010-11-19 16:12:05 -0500

    adding rubygem bundler

---
+++ src/examples/list_packages.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+$: <<  File.absolute_path(File.dirname(__FILE__) + "/../lib")
+
+require 'slackware'
+
+puts "tags used are: " + Slackware.tags_used.to_s
+
+pkg = "kernel-modules"
+if (Slackware.is_upgraded?(pkg))
+	puts pkg + " has been upgraded before!"
+	Slackware.upgrades(pkg).each {|up| puts up.inspect }
+else
+	puts pkg + " apparently has not ever been upgraded before"
+end
+
