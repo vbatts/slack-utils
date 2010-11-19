99d1c356e2f590d53c44dd399248305253fccd20
Vincent Batts <vbatts@hashbangbash.com>
2010-11-19 15:22:29 -0500

    more slackware class updates

--- src/lib/slackware.rb
+++ src/lib/slackware.rb
@@ -6,6 +6,8 @@ module Slackware
 			  data = File.read("/etc/slackware-version")
 			  data =~ /Slackware\s(.*)/
 			  $1
+		  rescue
+			  nil
 		  end
 
 	DIR_INSTALLED_PACKAGES = "/var/log/packages"
@@ -13,9 +15,10 @@ module Slackware
 	DIR_INSTALLED_SCRIPTS = "/var/log/scripts"
 	DIR_REMOVED_SCRIPTS = "/var/log/removed_scripts"
 	RE_REMOVED_NAMES = /^(.*)-upgraded-(\d{4}-\d{2}-\d{2}),(\d{2}:\d{2}:\d{2})$/
+	RE_BUILD_TAG = /^([[:digit:]]+)([[:alpha:]]+)$/
 
 	class Package
-		attr_accessor :fullname, :time, :path, :file, :name, :version, :arch, :build, :upgrade_time
+		attr_accessor :fullname, :time, :path, :file, :name, :version, :arch, :build, :tag, :upgrade_time
 		def initialize(name = nil)
 			self.name = name
 		end
@@ -31,7 +34,16 @@ module Slackware
 			end
 			self.fullname = name
 			arr = name.split('-').reverse
-			self.build = arr.shift
+			build = arr.shift
+			if (build.include?("_"))
+				self.build = build.split("_")[0]
+				self.tag = build.split("_")[1..-1].join("_")
+			elsif (build =~ RE_BUILD_TAG)
+				self.build = $1
+				self.tag = $2
+			else
+				self.build = build
+			end
 			self.arch = arr.shift
 			self.version = arr.shift
 			self.name = arr.reverse.join('-')
@@ -62,6 +74,7 @@ module Slackware
 				return DIR_INSTALLED_PACKAGE
 			end
 		end
+
 	end
 
 	class Script < Package
@@ -82,14 +95,37 @@ module Slackware
 	def self::installed_packages
 		return Dir.glob(DIR_INSTALLED_PACKAGES + "/*").sort.map {|p| Package.parse(p) }
 	end
+
 	def self::removed_packages
 		return Dir.glob(DIR_REMOVED_PACKAGES + "/*").sort.map {|p| Package.parse(p) }
 	end
+
 	def self::installed_scripts
 		return Dir.glob(DIR_INSTALLED_SCRIPTS + "/*").sort.map {|s| Script.parse(s) }
 	end
+
 	def self::removed_scripts
 		return Dir.glob(DIR_REMOVED_SCRIPTS + "/*").sort.map {|s| Script.parse(s) }
+	end
+
+	def self::tags_used
+		return installed_packages.map {|p| p.tag }.uniq.compact
+	end
+
+	def self::upgrades(pkg)
+		if (m = removed_packages.map {|p| p if (p.name == pkg) }.compact )
+			return m
+		else
+			return nil
+		end
+	end
+
+	def self::is_upgraded?(pkg)
+		if (removed_packages.map {|p| p.name if p.upgrade_time }.include?(pkg) )
+			return true
+		else
+			return false
+		end
 	end
 
 	def self::version
