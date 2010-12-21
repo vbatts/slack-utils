ef3b78d7e135875e68a82b5fbf0a8c1ef9a5206b
Vincent Batts <vbatts@hashbangbash.com>
2010-12-21 15:22:58 -0500

    adding another sample, it's snappy!

---
+++ src/examples/repo_difference.rb
@@ -0,0 +1,13 @@
+
+require 'slackware'
+
+pkgs = Slackware::System.installed_packages.map {|p| p.name }
+sr = Slackware::Repo.new
+sr.version = "current"
+c = sr.get_changelog
+
+printf("difference between current installation and %s...\n", sr.version)
+printf("%d should be removed\n", (pkgs & c[:removed].map {|p| p.name }).count)
+#p pkgs & c[:removed].map {|p| p.name }
+ca = c[:added].map {|p| p.name }
+printf("%d should be added\n", (ca.count - (pkgs & ca).count))
