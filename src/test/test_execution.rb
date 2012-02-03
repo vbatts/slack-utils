df787e969eb51f21c46224a6b7d9a1b046f3e742
Vincent Batts <vbatts@hashbangbash.com>
2012-02-03 10:28:55 -0500

    updating the execution tests issue #9

--- src/test/test_execution.rb
+++ src/test/test_execution.rb
@@ -21,7 +21,7 @@ class TestExecution < Test::Unit::TestCase
 
   def test_slp
      res = nil
-     IO.popen("ruby %s/slp bash" % $bin_path) {|io| res = io.read }
+     IO.popen("ruby %s/slp -D bash" % $bin_path) {|io| res = io.read }
      pid = $?
      assert_equal(2, res.split("\n").length, "the command did not return the correct number of results")
      assert_equal(true, pid.success?, "the process did not return successfully")
@@ -29,9 +29,17 @@ class TestExecution < Test::Unit::TestCase
 
   def test_slf
      res = nil
-     IO.popen("ruby %s/slf bin/bash" % $bin_path) {|io| res = io.read }
+     IO.popen("ruby %s/slf -D bin/bash" % $bin_path) {|io| res = io.read }
      pid = $?
      assert_equal("bin/bash4.new", res.split(": ").last.chomp, "the expected file was not found")
+     assert_equal(true, pid.success?, "the process did not return successfully")
+  end
+
+  def test_slo
+     res = nil
+     IO.popen("ruby %s/slo -D" % $bin_path) {|io| res = io.read }
+     pid = $?
+     assert_equal("", res.chomp, "the expected file was not found")
      assert_equal(true, pid.success?, "the process did not return successfully")
   end
 end
